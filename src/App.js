270785a794501c6f5523ab45953cd4261c2270db
Samet İrkören <sametirkoren@gmail.com>
2019-12-25 20:25:31 +0300

    create navbar component and install node-sass ,  fontawesome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/Navbar/navbar.scss';
+import Navbar from './components/Navbar/Navbar';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <Navbar/>
+    </React.Fragment>
   );
 }
 
